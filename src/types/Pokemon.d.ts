91f48bd64d40ab616aa4ac4b4312db69faa79993
adiandoyo32 <akhiang32@gmail.com>
2024-02-02 01:45:42 +0700

    feat: pokemon detail and pokemon species type

--- src/types/Pokemon.d.ts
+++ src/types/Pokemon.d.ts
@@ -16,4 +16,35 @@ declare namespace Pokemon {
     name: string
     image?: string
   }
+
+  type PokemonDetail = {
+    abilities?: Ability[]
+    base_experience?: number
+    forms?: Pokemon[]
+    game_indices?: GameIndex[]
+    height?: number
+    held_items?: HeldItem[]
+    id?: number
+    is_default?: boolean
+    location_area_encounters?: string
+    moves?: Move[]
+    name?: string
+    order?: number
+    species?: Pokemon
+    sprites?: Sprite
+    stats?: Stat[]
+    types?: Type[]
+    weight?: number
+    status?: boolean
+    message?: string
+  }
+
+  type PokemonSpecies = {
+    base_happiness?: number
+    capture_rate?: number
+    color?: {
+      name: string
+      url: string
+    }
+  }
 }
